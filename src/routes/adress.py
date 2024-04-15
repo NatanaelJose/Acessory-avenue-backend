4b1720dd4dbbd06cdf4b09cc40f74adba0c41147
NatanaelJose <josesaraivamaciel@gmail.com>
2024-04-15 14:44:07 -0300

    FIX: corrige o nome da rota

--- src/routes/adress.py
+++ src/routes/adress.py
@@ -3,8 +3,8 @@ from src.errors.user_not_found_exception import UserNotFoundException
 from src.static.messages import NOT_FOUND, INTERNAL_ERROR
 
 
-api_router = APIRouter(prefix="/addres")
+api_router = APIRouter(prefix="/adress")
 
 @api_router.get("", status_code=status.HTTP_200_OK)
-def list_addres():
+def list_adress():
     return 'ok'
